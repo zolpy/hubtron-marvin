e124427c9e7cc834e8771b9eb597feeb0365cf0a
Brandão <brandao.jlc@gmail.com>
2022-04-11 14:53:53 -0300

    Add files via upload

---
+++ hubtron/mapa/About.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+def About():
+    # st.subheader("Residencia Hub IA/ Fiep / SENAI-PR")
+    # st.image('https://res.cloudinary.com/dmbamuk26/image/upload/v1640886909/images/banner_hub.jpg', output_format="auto")
+    # components.html("""
+    #         <div style="text-align: center;"><a href="https://www.senaipr.org.br/tecnologiaeinovacao/nossarede/hubia/"><img src="https://cdn3.iconfinder.com/data/icons/social-media-circle-long-shadow/1024/longShadow-256.png" alt="" width="36" height="36" /> </a>
+    #         <a href="https://br.linkedin.com/company/hubiasenai"><img src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Linkedin_unofficial_colored_svg-512.png" alt="" width="36" height="36" /> </a>
+    #         </div>
+    #         """)
+    # ############################################################
+    # st.subheader("Residente Andre Mikio")
+    # st.image('https://res.cloudinary.com/dmbamuk26/image/upload/v1646767488/images/banner_andre.jpg', output_format="auto")
+    # components.html("""
+    #         <div style="text-align: center;">
+    #         <a href="https://github.com/AMikio"><img src="https://cdn3.iconfinder.com/data/icons/social-media-2169/24/social_media_social_media_logo_github_2-512.png" alt="" width="36" height="36" /> </a>
+    #         <a href="https://www.linkedin.com/in/andr%C3%A9-shimonishi-697516a2/"><img src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Linkedin_unofficial_colored_svg-512.png" alt="" width="36" height="36" /> </a>
+    #            """)
+    # ############################################################
+    # st.subheader("Residente Luiz Brandão (Soldado)")
+    # index.carrega('Files/Code/index.html')
+    # st.info("Built with Streamlit")
+    st.info("Brandão, Luiz @uflaniano")
+    st.image('https://res.cloudinary.com/dmbamuk26/image/upload/v1640886909/images/banner_brandao.jpg', output_format="auto")
+    components.html("""
+            <div style="text-align: center;">
+            <a href="https://www.uflaniano.com.br/"><img src="https://cdn3.iconfinder.com/data/icons/social-media-circle-long-shadow/1024/longShadow-256.png" alt="" width="36" height="36" /> </a> 
+            <a href="http://buscatextual.cnpq.br/buscatextual/visualizacv.do?id=K4817683Y6"><img src="https://cdn2.iconfinder.com/data/icons/bitsies/128/EditDocument-512.png" alt="" width="36" height="36" /> </a> 
+            <a href="https://github.com/zolpy"><img src="https://cdn3.iconfinder.com/data/icons/social-media-2169/24/social_media_social_media_logo_github_2-512.png" alt="" width="36" height="36" /> </a> 
+            <a href="https://www.linkedin.com/in/luiz-carlos-brand%C3%A3o-junior-605b94b1/"><img src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Linkedin_unofficial_colored_svg-512.png" alt="" width="36" height="36" /> </a> 
+            <a href="https://twitter.com/uflaniano"><img src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Twitter3_colored_svg-512.png" alt="" width="36" height="36" /> </a> 
+            <a href="https://www.facebook.com/uflaniano"><img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/square-facebook-512.png" alt="" width="36" height="36" /> </a> 
+            <a href="https://www.instagram.com/uflaniano/"><img src="https://cdn2.iconfinder.com/data/icons/social-media-applications/64/social_media_applications_3-instagram-512.png" alt="" width="36" height="36" /> </a> 
+            <a href="https://br.pinterest.com/uflaniano/"><img src="https://cdn2.iconfinder.com/data/icons/2018-social-media-app-logos/1000/2018_social_media_popular_app_logo_pinterest-512.png" alt="" width="36" height="36" /> </a> 
+            <a href="https://www.tiktok.com/@uflaniano"><img src="https://cdn4.iconfinder.com/data/icons/logos-brands-in-colors/2840/tiktok-logo-512.png" alt="" width="36" height="36" /> </a> 
+            <a href="https://www.youtube.com/channel/UCc6XH0e8US45u5IkoF6Ul0A"><img src="https://cdn4.iconfinder.com/data/icons/socialcones/508/YouTube-512.png" alt="" width="36" height="36" /> </a> 
+            <a href="https://hub.docker.com/u/uflaniano"><img src="https://cdn3.iconfinder.com/data/icons/social-media-2169/24/social_media_social_media_logo_docker-512.png" alt="" width="36" height="36" /> </a></div>
+            """)
---
+++ hubtron/mapa/Marvin.py
@@ -0,0 +1,84 @@
+# import pyaudio as pa
+import speech_recognition as sr
+import datetime
+import pyttsx3
+import wikipedia
+import pywhatkit
+import streamlit as st
+
+recognizer = sr.Recognizer()
+audio = sr.Recognizer()
+maquina = pyttsx3.init()
+
+st.set_page_config(
+     page_title="Marvin",
+     page_icon="https://cdn0.iconfinder.com/data/icons/monster-14/64/cyborg_halloween_machine_robot_teen_-256.png",
+     layout="wide",
+     initial_sidebar_state="expanded",
+     menu_items={
+         'Get Help': 'https://www.extremelycoolapp.com/help',
+         'Report a bug': "https://www.extremelycoolapp.com/bug",
+         'About': "# This is a header. This is an *extremely* cool app!"
+     }
+ )
+
+# Header
+st.image('https://res.cloudinary.com/dmbamuk26/image/upload/v1649696840/hubtron/hubtron_marvin.gif')
+maquina.say(
+    'Olá, seu sou o primeiro Hubtron, meu nome é Marvin. Meus sintetizadores de voz não estão ajudando muito, sinto muito por isso. Em que eu posso lhe ser útil?')
+st.write(
+    'Olá, seu sou o primeiro Hubtron, meu nome é Marvin. Meus sintetizadores de voz não estão ajudando muito, sinto muito por isso. Em que eu posso lhe ser útil?')
+maquina.runAndWait()
+
+st.markdown("### Os comandos são simples")
+
+st.markdown("Apenas use: ")
+st.write("Marvin + relógio : para saber as horas.")
+st.write("Marvin + procurar + COISA QUE SE QUEIRA: para saber sobre alguma coisa.")
+st.write("Marvin + toque + NOME DA MÚSICA QUE SE QUEIRA: para abrir algum video do YouTube")
+
+def chamaMarvin():
+    with sr.Microphone() as source:  # microphone as source
+        audio = recognizer.listen(source, None, 3)  # listen for the audio via source
+        voice_data = ''
+
+        try:
+            voice_data = recognizer.recognize_google(audio, language="pt-BR").lower()  # convert audio to text
+        except sr.UnknownValueError:  # error: recognizer does not understand
+            print()
+        except sr.RequestError:
+            print('Serviço offline')  # error: recognizer is not connected
+
+        print(">>", voice_data)  # print what user saidprint
+        # for bot_name_variation in BOT_NAMES:
+        if (('marvin') in voice_data):
+            print(">>>", voice_data)  # print what use
+            st.write(">>>", voice_data)  # print what user saidprint
+            if ('relógio' in voice_data):
+                # if 'hora' in voice_data:
+                # voice_data = voice_data.replace('marvin', '')
+                print(">>>>", voice_data)  # print what user saidprint
+                st.write(">>>>", voice_data)  # print what user saidprint
+                hora = datetime.datetime.now().strftime('%H:%M')
+                maquina.say('Agora são' + hora)
+                st.write(">>>> Agora são " + hora)
+                maquina.runAndWait()
+            elif 'procurar' in voice_data:
+                procurar = voice_data.replace('procurar', '')
+                wikipedia.set_lang("pt")
+                print(">>>>", voice_data)  # print what user saidprint
+                resultado = wikipedia.summary(procurar, 2)
+                print(resultado)
+                st.write(resultado)
+                maquina.say(resultado)
+                maquina.runAndWait()
+
+            elif 'toque' in voice_data:
+                musica = voice_data.replace('toque', '')
+                resultado = pywhatkit.playonyt(musica)
+                maquina.say(resultado)
+                maquina.runAndWait()
+
+
+while True:
+    chamaMarvin()
